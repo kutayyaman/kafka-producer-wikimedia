2cb3ee275cd147f42c847c3f16068d19c1ccfdb7
kutayyaman <yamankutay1@gmail.com>
2022-09-08 21:06:18 +0300

    KafkaTopicConfig is done

---
+++ src/main/java/com/kutayyaman/kafkaproducerwikimedia/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package com.kutayyaman.kafkaproducerwikimedia.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    private static final String WIKIMEDIA_TOPIC= "wikimedia_topic";
+
+    @Bean
+    public NewTopic wikimediaTopic(){
+        return TopicBuilder.name(this.WIKIMEDIA_TOPIC)
+                .build();
+    }
+}
