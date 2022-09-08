7388b562e2b78105bd6bf93058f910e3bf3c0e9c
kutayyaman <yamankutay1@gmail.com>
2022-09-08 21:33:46 +0300

    WIKIMEDIA_TOPIC field's accesser type is changed as public

--- src/main/java/com/kutayyaman/kafkaproducerwikimedia/config/KafkaTopicConfig.java
+++ src/main/java/com/kutayyaman/kafkaproducerwikimedia/config/KafkaTopicConfig.java
@@ -7,7 +7,7 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
-    private static final String WIKIMEDIA_TOPIC= "wikimedia_recent_change_topic";
+    public static final String WIKIMEDIA_TOPIC= "wikimedia_recent_change_topic";
 
     @Bean
     public NewTopic wikimediaTopic(){
